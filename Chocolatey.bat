0aef09884c195306516ad85e4f7910bed11c5dc3
Ryan Horiguchi <rhoriguchi@users.noreply.github.com>
2018-05-27 23:08:54 +0200

    Update choco packages

--- Chocolatey.bat
+++ Chocolatey.bat
@@ -6,6 +6,7 @@ choco install cheatengine
 choco install chocolatey
 choco install chocolatey-core.extension
 choco install chocolatey-windowsupdate.extension
+choco install DotNet4.5.2
 choco install DotNet4.6.1
 choco install driverbooster
 choco install fileshredder
@@ -21,9 +22,11 @@ choco install GoogleChrome
 choco install handbrake.install
 choco install hearthstone-deck-tracker
 choco install hearthstone-deck-tracker-arena-helper
+choco install hyper
 choco install InkScape
 choco install intellijidea-ultimate
 choco install jdk8
+choco install jdownloader
 choco install jre8
 choco install KB2919355
 choco install KB2919442
@@ -55,6 +58,7 @@ choco install tomcat
 choco install tor-browser
 choco install vcredist140
 choco install vcredist2015
+choco install veeam-agent
 choco install vivaldi
 choco install vlc
 choco install WhatsApp
