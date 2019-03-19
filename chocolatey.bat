df16d5f305661ec2c693d9428ed8972afdd39c90
Ryan Horiguchi <rhoriguchi@users.noreply.github.com>
2019-03-19 18:05:39 +0100

    Update chocolatey.bat

--- chocolatey.bat
+++ chocolatey.bat
@@ -1,7 +1,7 @@
-choco install docker-desktop
-choco install dotnetcore-sdk
-choco install git.install
-choco install jdk11
-choco install maven
-choco install nodejs.install
-choco install python3
+choco install -y docker-desktop
+choco install -y dotnetcore-sdk
+choco install -y git.install
+choco install -y jdk11
+choco install -y maven
+choco install -y nodejs.install
+choco install -y python3
